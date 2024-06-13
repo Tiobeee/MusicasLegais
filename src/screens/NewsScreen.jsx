e390ef8e1dc637c60819de0ba0325fd859df7562
Tiobee <bernardo.budal10@gmail.com>
2024-06-13 16:57:11 -0300

    omg

--- src/screens/NewsScreen.jsx
+++ src/screens/NewsScreen.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, FlatList } from "react-native";
+
 export default function NewsScreen() {
   const [news, setNews] = useState([]);
+
   useEffect(() => {
     fetch(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_NEWS_API_KEY"
+      "https://api.currentsapi.services/v1/search?keywords=musica&language=pt&apiKey=wTylkzOXniX2s-EzsxjQ9OQTif_CSy6TMfn3EO-ieoNnrf7_"
     )
       .then((response) => response.json())
-      .then((data) => setNews(data.articles))
+      .then((data) => setNews(data.news))
       .catch((error) => console.error(error));
   }, []);
+
   return (
     <View>
-      {" "}
       <FlatList
         data={news}
         keyExtractor={(item) => item.url}
